22914bc4b05ab4c611b944c365291aa64a254e64
Rounak <rounakverma30march@gmail.com>
2025-03-12 01:29:06 +0530

    remove_remberer

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -5,14 +5,12 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from accounts.views import reset_password
 
-
 urlpatterns = [
     path('accounts/', include('accounts.urls')),  # Fixed quotes and missing comma
-        path("reset-password/<str:token>/", reset_password, name="reset_password"),
-
     path('product/', include('products.urls')), 
     path('admin/', admin.site.urls),
     path('', include('home.urls')), 
+    path("reset-password/<str:token>/", reset_password, name="reset_password"),
     
 ]
 
